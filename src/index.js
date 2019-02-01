64e142c70ecbc8baa2ff2238112d1e7c09675ff9
Kristine Doctor <mskristinedoctor@gmail.com>
2019-02-01 13:48:10 -0800

    fix export

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import { initAspera, asperaWeb, asperaInstaller, config } from './react-aspera-connect';
 import { withAspera }  from './with-aspera';
 
-exports.initAspera = initAspera;
-exports.withAspera = withAspera;
-exports.aspera = {
-    web: asperaWeb, 
-    installer: asperaInstaller, 
-    config: config
-}
+
+
+export const aspera = {
+        initAspera: initAspera,
+        withAspera: withAspera,
+        aspera: {
+            web: asperaWeb, 
+            installer: asperaInstaller, 
+            config: config
+        }
+    }
