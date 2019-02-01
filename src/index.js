b121069f8c0031af35f2ca9a6d4e28e1d269bd09
Kristine Doctor <mskristinedoctor@gmail.com>
2019-02-01 14:51:44 -0800

    ......

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import { initAspera, asperaWeb, asperaInstaller, config } from './react-aspera-connect';
 import { withAspera }  from './with-aspera';
 
+const awesomeFunction = () => 'I am just an Awesome Function';
 
 
-module.exports = {
-        initAspera: initAspera,
-        withAspera: withAspera,
-        aspera: {
-            web: asperaWeb, 
-            installer: asperaInstaller, 
-            config: config
-        }
-    }
+
+// module.exports = {
+//         x: 4,
+//         initAspera: initAspera,
+//         withAspera: withAspera,
+//         aspera: {
+//             web: asperaWeb, 
+//             installer: asperaInstaller, 
+//             config: config
+//         }
+//     }
+
+export { awesomeFunction, initAspera, withAspera, asperaWeb, asperaInstaller, config };
