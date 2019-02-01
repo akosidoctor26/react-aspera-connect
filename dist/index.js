29c3bf97a255701d845400db69ac2058172de7f9
Kristine Doctor <mskristinedoctor@gmail.com>
2019-02-01 13:29:16 -0800

    add dist

--- dist/index.js
+++ dist/index.js
@@ -63,7 +63,7 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 9);
+/******/ 	return __webpack_require__(__webpack_require__.s = 10);
 /******/ })
 /************************************************************************/
 /******/ ([
@@ -261,10 +261,10 @@ process.umask = function() { return 0; };
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return config; });
-/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "b", function() { return asperaInstaller; });
-/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "c", function() { return asperaWeb; });
-/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "d", function() { return initAspera; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "d", function() { return config; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "c", function() { return asperaInstaller; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "b", function() { return asperaWeb; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return initAspera; });
 function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; var ownKeys = Object.keys(source); if (typeof Object.getOwnPropertySymbols === 'function') { ownKeys = ownKeys.concat(Object.getOwnPropertySymbols(source).filter(function (sym) { return Object.getOwnPropertyDescriptor(source, sym).enumerable; })); } ownKeys.forEach(function (key) { _defineProperty(target, key, source[key]); }); } return target; }
 
 function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
@@ -403,10 +403,40 @@ module.exports = shouldUseNative() ? Object.assign : function (target, source) {
 
 /***/ }),
 /* 3 */
+/***/ (function(module, exports) {
+
+module.exports = function(originalModule) {
+	if(!originalModule.webpackPolyfill) {
+		var module = Object.create(originalModule);
+		// module.parent = undefined by default
+		if(!module.children) module.children = [];
+		Object.defineProperty(module, "loaded", {
+			enumerable: true,
+			get: function() {
+				return module.l;
+			}
+		});
+		Object.defineProperty(module, "id", {
+			enumerable: true,
+			get: function() {
+				return module.i;
+			}
+		});
+		Object.defineProperty(module, "exports", {
+			enumerable: true,
+		});
+		module.webpackPolyfill = 1;
+	}
+	return module;
+};
+
+
+/***/ }),
+/* 4 */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_react__ = __webpack_require__(8);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_react__ = __webpack_require__(9);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_react___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0_react__);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__react_aspera_connect__ = __webpack_require__(1);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return withAspera; });
@@ -449,9 +479,9 @@ var withAspera = function withAspera(WrappedComponent) {
         value: function render() {
           return __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(WrappedComponent, _extends({
             aspera: {
-              web: __WEBPACK_IMPORTED_MODULE_1__react_aspera_connect__["c" /* asperaWeb */],
-              installer: __WEBPACK_IMPORTED_MODULE_1__react_aspera_connect__["b" /* asperaInstaller */],
-              config: __WEBPACK_IMPORTED_MODULE_1__react_aspera_connect__["a" /* config */]
+              web: __WEBPACK_IMPORTED_MODULE_1__react_aspera_connect__["b" /* asperaWeb */],
+              installer: __WEBPACK_IMPORTED_MODULE_1__react_aspera_connect__["c" /* asperaInstaller */],
+              config: __WEBPACK_IMPORTED_MODULE_1__react_aspera_connect__["d" /* config */]
             }
           }, this.props));
         }
@@ -463,7 +493,7 @@ var withAspera = function withAspera(WrappedComponent) {
 };
 
 /***/ }),
-/* 4 */
+/* 5 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -479,7 +509,7 @@ var withAspera = function withAspera(WrappedComponent) {
 var printWarning = function() {};
 
 if (process.env.NODE_ENV !== 'production') {
-  var ReactPropTypesSecret = __webpack_require__(5);
+  var ReactPropTypesSecret = __webpack_require__(6);
   var loggedTypeFailures = {};
 
   printWarning = function(text) {
@@ -562,7 +592,7 @@ module.exports = checkPropTypes;
 /* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(0)))
 
 /***/ }),
-/* 5 */
+/* 6 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -581,7 +611,7 @@ module.exports = ReactPropTypesSecret;
 
 
 /***/ }),
-/* 6 */
+/* 7 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -603,7 +633,7 @@ if (process.env.NODE_ENV !== "production") {
 'use strict';
 
 var _assign = __webpack_require__(2);
-var checkPropTypes = __webpack_require__(4);
+var checkPropTypes = __webpack_require__(5);
 
 // TODO: this is special because it gets imported during build.
 
@@ -2472,7 +2502,7 @@ module.exports = react;
 /* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(0)))
 
 /***/ }),
-/* 7 */
+/* 8 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -2503,35 +2533,43 @@ unstable_ConcurrentMode:x,unstable_Profiler:u,__SECRET_INTERNALS_DO_NOT_USE_OR_Y
 
 
 /***/ }),
-/* 8 */
+/* 9 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
 /* WEBPACK VAR INJECTION */(function(process) {
 
 if (process.env.NODE_ENV === 'production') {
-  module.exports = __webpack_require__(7);
+  module.exports = __webpack_require__(8);
 } else {
-  module.exports = __webpack_require__(6);
+  module.exports = __webpack_require__(7);
 }
 
 /* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(0)))
 
 /***/ }),
-/* 9 */
+/* 10 */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* WEBPACK VAR INJECTION */(function(module) {Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__react_aspera_connect__ = __webpack_require__(1);
-/* harmony namespace reexport (by provided) */ __webpack_require__.d(__webpack_exports__, "config", function() { return __WEBPACK_IMPORTED_MODULE_0__react_aspera_connect__["a"]; });
-/* harmony namespace reexport (by provided) */ __webpack_require__.d(__webpack_exports__, "asperaInstaller", function() { return __WEBPACK_IMPORTED_MODULE_0__react_aspera_connect__["b"]; });
-/* harmony namespace reexport (by provided) */ __webpack_require__.d(__webpack_exports__, "asperaWeb", function() { return __WEBPACK_IMPORTED_MODULE_0__react_aspera_connect__["c"]; });
-/* harmony namespace reexport (by provided) */ __webpack_require__.d(__webpack_exports__, "initAspera", function() { return __WEBPACK_IMPORTED_MODULE_0__react_aspera_connect__["d"]; });
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__with_aspera__ = __webpack_require__(3);
-/* harmony namespace reexport (by provided) */ __webpack_require__.d(__webpack_exports__, "withAspera", function() { return __WEBPACK_IMPORTED_MODULE_1__with_aspera__["a"]; });
+/* harmony reexport (binding) */ __webpack_require__.d(__webpack_exports__, "initAspera", function() { return __WEBPACK_IMPORTED_MODULE_0__react_aspera_connect__["a"]; });
+/* harmony reexport (binding) */ __webpack_require__.d(__webpack_exports__, "asperaWeb", function() { return __WEBPACK_IMPORTED_MODULE_0__react_aspera_connect__["b"]; });
+/* harmony reexport (binding) */ __webpack_require__.d(__webpack_exports__, "asperaInstaller", function() { return __WEBPACK_IMPORTED_MODULE_0__react_aspera_connect__["c"]; });
+/* harmony reexport (binding) */ __webpack_require__.d(__webpack_exports__, "config", function() { return __WEBPACK_IMPORTED_MODULE_0__react_aspera_connect__["d"]; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__with_aspera__ = __webpack_require__(4);
+/* harmony reexport (binding) */ __webpack_require__.d(__webpack_exports__, "withAspera", function() { return __WEBPACK_IMPORTED_MODULE_1__with_aspera__["a"]; });
 
 
+module.exports = {
+  initAspera: initAspera,
+  withAspera: withAspera,
+  asperaWeb: asperaWeb,
+  asperaInstaller: asperaInstaller,
+  config: config
+};
+/* WEBPACK VAR INJECTION */}.call(__webpack_exports__, __webpack_require__(3)(module)))
 
 /***/ })
 /******/ ]);
