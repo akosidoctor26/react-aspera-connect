e28e9ca35ed2853b2f2a485355a4995fcb0ef823
Kristine Doctor <mskristinedoctor@gmail.com>
2019-02-01 13:26:58 -0800

    fix bug

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
-export * from './react-aspera-connect';
-export *  from './with-aspera';
+export { initAspera, asperaWeb, asperaInstaller, config } from './react-aspera-connect';
+export { withAspera }  from './with-aspera';
+
+module.exports = {
+    initAspera,
+    withAspera,
+    asperaWeb, asperaInstaller, config
+}
